de08f9e5d0a125912908525049598e8e58320dd0
Chris Alfano <chris@jarv.us>
2017-01-10 19:54:23 -0500

    Remove autoDecode default value

--- src/proxy/API.js
+++ src/proxy/API.js
@@ -166,7 +166,6 @@ Ext.define('Jarvus.proxy.API', {
         var me = this;
 
         request.setRawRequest(me.getConnection().request(Ext.applyIf({
-            autoDecode: null,
             failureStatusCodes: [404] // TODO: verify this results in the proper failure method being called in the proxy
         }, request.getCurrentConfig())));
 
